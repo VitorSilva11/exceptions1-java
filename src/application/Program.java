1d3c88d35c262ed1a8fd063c93c25758f5389cf3
Vitor Silva <svitoryago@hotmail.com>
2022-03-23 11:35:05 -0300

    Versao ruim de tratamento de exception

--- src/application/Program.java
+++ src/application/Program.java
@@ -8,52 +8,44 @@ import java.util.Scanner;
 import model.entities.Reservation;
 
 public class Program {
-	
+
 	public static void main(String[] args) throws ParseException {
-		
+
 		Scanner sc = new Scanner(System.in);
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		
+
 		System.out.print("Room Number: ");
 		int roomNumber = sc.nextInt();
 		System.out.print("Check-In date (DD/MM/YYYY): ");
 		Date checkIn = sdf.parse(sc.next());
 		System.out.print("Check-Out date (DD/MM/YYYY): ");
 		Date checkOut = sdf.parse(sc.next());
-		
-		
-		
-		if(!checkOut.after(checkIn)) {
+
+		if (!checkOut.after(checkIn)) {
 			System.out.println("Error in reservation: Check-Out date must be after Check-In date");
-		}else {
+		} else {
 			Reservation reservation = new Reservation(roomNumber, checkIn, checkOut);
 			System.out.println("Reservation: " + reservation);
-			
+
 			System.out.println();
 			System.out.println("Enter date to update the reservation: ");
 			System.out.print("Check-In date (DD/MM/YYYY): ");
 			checkIn = sdf.parse(sc.next());
 			System.out.print("Check-Out date (DD/MM/YYYY): ");
 			checkOut = sdf.parse(sc.next());
-			
-			Date now = new Date();
-			
-			if(checkIn.before(now) || checkOut.before(now)) {
-				System.out.println("Error in reservation: Reservation dates for updates must be future dates");
-			}else if (!checkOut.after(checkIn)) {
-				System.out.println("Error in reservation: Check-Out date must be after Check-In date");
-			}else {
-				reservation.updateDates(checkIn, checkOut);
+
+			String erro = reservation.updateDates(checkIn, checkOut);
+
+			if (erro != null) {
+				System.out.println("Error in reservation: " + erro);
+			} else {
 				System.out.println("Reservation Update: " + reservation);
 			}
-			
+
+			sc.close();
+
 		}
-		
-		
-		
-		sc.close();
-		
-		
+
 	}
 
 }
